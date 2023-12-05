70cb71ec78c591c0022d26c7d3ae94cbcdd8aa7e
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2023-12-05 16:51:28 -0600

    Working API

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,35 @@
-
+let cityInputEl = document.getElementById('city-input');
 
 let fetchButton = document.getElementById('fetch-btn')
+// const APIKey = '48428fb78e045d81cb3a78148a22a8a9'
 
-function getWeather() {
-    let requestUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-
-fetch(requestUrl)
-    .then(function(response) {
+function getData() {
+    let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=austin&appid=48428fb78e045d81cb3a78148a22a8a9';
+    // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+    // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+    let cityName = cityInputEl.value;
+  
+fetch (requestUrl)
+    .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
-        for (let i = 0; i < data.length; i++) {
-            let date = document.createElement('h3')
-            let lowTemp = document.createElement('')
-            let highTemp = document.createElement('')
-            let currentTemp = document.createElement('')
-            date.textContent = data[i].date
-            lowTemp.textContent = data[i].lowTemp
-            highTemp.textContent = data[i].highTemp
-            currentTemp.textContent = data[i].currentTemp
-            weatherContainer.append(date);
-            weatherContainer.append(lowTemp);
-            weatherContainer.append(highTemp);
-            weatherContainer.append(currentTemp);
-        }
-    });
-};
+        console.log(data)
+        // for (var i = 0; i < data.length; i++) {
+            let date = document.createElement('p');
+            let lowTemp = document.createElement('p');
+            let highTemp = document.createElement('p');
+            let description = document.createElement('p');
+            date.textContent = data.city;
+            lowTemp.textContent = data.main.temp_min;
+            highTemp.textContent = data.main.temp_max;
+            description.textContent = data.description;
+            day1.append(date);
+            day1.append(lowTemp);
+            day1.append(highTemp);
+            day1.append(description);
+        // }
+    })
+}
 
-fetchButton.addEventListener('click', getWeather);
+fetchButton.addEventListener('click', getData);
